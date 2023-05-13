0ac7173a422edde0aa9648eddbc17b15d290c173
Vladimir <107151383+Vladimir2106@users.noreply.github.com>
2023-05-13 20:04:44 +0300

    Add files via upload

---
+++ loop.php
@@ -0,0 +1,36 @@
+<h4>
+	НАЧАЛО Тестовие поля через вівод постов + функции ВОРДПРЕС
+</h4>
+
+<section>
+	     <div class="box362">
+			 <?php
+			 global $post;
+			 $myposts=get_posts([
+				 'numberpost'=>-1,
+			 ]);
+			 if($myposts){
+				 foreach($myposts as $post){
+					 setup_postdata($post);
+					 ?>
+			    <div class="box351">
+                <div>
+                    <div class="box37" id="b6"><?php the_title(); ?></div>
+                    <div class="box393"  id="b5"><?php the_content(); ?></div>
+                </div>
+                <div>
+                    <div class="box361">
+                        <img src=<?php the_post_thumbnail(array(250, 250), array('class'=>'box741'))?>>;
+                    </div>
+                </div> 
+            </div>
+			 <?php
+				 }
+			 }else{
+			 echo 'карточек товара нет';
+			 }
+			 wp_reset_postdata();
+			 ?>
+         </div> 
+</section>
+<h4> Конец Тестовие поля через вівод постов + функции ВОРДПРЕС </h4>
